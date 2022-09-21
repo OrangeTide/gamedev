98f23cae0ad384cd52c310d4b7b172bbff666049
Jon Mayo <jon@rm-f.net>
2022-09-21 07:40:10 -0700

    add more gamepad patches

--- src/sokol/update-source.sh
+++ src/sokol/update-source.sh
@@ -9,5 +9,8 @@ curl -O https://raw.githubusercontent.com/floooh/sokol/master/sokol_app.h \
 	-O https://raw.githubusercontent.com/floooh/sokol/master/util/sokol_imgui.h
 
 # Apply patches
+rm sokol_gamepad.h
 patch -p1 < 0001-gamepad-API.patch
 patch -p1 < 0002-sokol_app-changes-for-android-support.patch
+patch -p1 < 0003-fix-initializer-in-gamepad-API.patch
+patch -p1 < 0004-load-XInput-library-in-gamepad-API.patch
