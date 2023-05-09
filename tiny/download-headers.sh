9d98f56e04947192d43879816d456810c7d65e3c
Jon Mayo <jon@rm-f.net>
2023-05-08 20:58:46 -0700

    tiny updates

--- tiny/download-headers.sh
+++ tiny/download-headers.sh
@@ -1,18 +1,43 @@
 #!/bin/sh
-set -xe
+set -e
+
+HEADERS=(
 # OpenGL ES 2.0
-GLES2_HEADERS=("https://registry.khronos.org/OpenGL/api/GLES2/gl2.h" "https://registry.khronos.org/OpenGL/api/GLES2/gl2ext.h" "https://registry.khronos.org/OpenGL/api/GLES2/gl2platform.h")
-mkdir -p include/GLES2
-cd include/GLES2
-for f in "${GLES2_HEADERS[@]}" ; do
-  curl -O $f
-done
-cd -
+"https://registry.khronos.org/OpenGL/api/GLES2/gl2.h"
+"https://registry.khronos.org/OpenGL/api/GLES2/gl2ext.h"
+"https://registry.khronos.org/OpenGL/api/GLES2/gl2platform.h"
 # OpenGL ES 3.0, 3.1, and 3.2
-GLES3_HEADERS=("https://registry.khronos.org/OpenGL/api/GLES3/gl32.h" "https://registry.khronos.org/OpenGL/api/GLES3/gl31.h" "https://registry.khronos.org/OpenGL/api/GLES3/gl3.h" "https://registry.khronos.org/OpenGL/api/GLES3/gl3platform.h")
-mkdir -p include/GLES3
-cd include/GLES3
-for f in "${GLES2_HEADERS[@]}" ; do
-  curl -O $f
+"https://registry.khronos.org/OpenGL/api/GLES3/gl32.h"
+"https://registry.khronos.org/OpenGL/api/GLES3/gl31.h"
+"https://registry.khronos.org/OpenGL/api/GLES3/gl3.h"
+"https://registry.khronos.org/OpenGL/api/GLES3/gl3platform.h"
+# Khronos EGL
+"https://registry.khronos.org/EGL/api/EGL/egl.h"
+"https://registry.khronos.org/EGL/api/EGL/eglext.h"
+"https://registry.khronos.org/EGL/api/EGL/eglplatform.h"
+"https://registry.khronos.org/EGL/api/KHR/khrplatform.h"
+)
+
+for url in "${HEADERS[@]}" ; do
+  B="${url##*://*/api/}"
+  if [ ! -e "include/$B" ]; then
+    echo "Downloading $url ..."
+    curl -sS -L --create-dirs --output "include/$B" "$url"
+  else
+    echo "[SKIP] Downloading $url ..."
+  fi
 done
-cd -
+
+cat <<EOF | sha1sum --check
+108a22ccb13ea471940804584e01fbefa78a4872  include/EGL/eglext.h
+30b8964110af967b8ca07ef4530f56a88d849c21  include/EGL/egl.h
+60963b845b8d7570520c853f5e0a36a645fd3cbc  include/EGL/eglplatform.h
+0e8f5f850f65c859faf561f8495a83ecd13b7030  include/GLES2/gl2ext.h
+8243af14859460eb33bfc933687dc8388fab0d3b  include/GLES2/gl2.h
+39b2372c3d254addee407682f44760ca22116c7e  include/GLES2/gl2platform.h
+eb90411ed714acc684c225e1ae7d3105e6a0a9e3  include/GLES3/gl31.h
+44cc0f878a3eef929fbc576ff7b9e614e005a351  include/GLES3/gl32.h
+00a12f93da92c9d0d1dfd0842c7fcfc070773404  include/GLES3/gl3.h
+a032fbbd07a90fd378a154834d3dc6ce93ee198d  include/GLES3/gl3platform.h
+9818ea12cfb96c60bdbb5103d8cf7754dcce34f4  include/KHR/khrplatform.h
+EOF
--- tiny/tiny.c
+++ tiny/tiny.c
@@ -1,348 +1,244 @@
-// Windows Only
-// TODO: add Linux/EGL support
-#ifndef UNICODE
-# define UNICODE
-# define _UNICODE
+#ifdef UNICODE
+#ifndef _UNICODE
+#define _UNICODE
 #endif
-#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 #include <tchar.h>
+#include <EGL/egl.h>
+#include <EGL/eglplatform.h>
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
+#include <stdio.h>
+#include <stdarg.h>
 
-#define TINYLIB_OK (0)
-#define TINYLIB_ERR (-1)
+#define ERR (-1)
+#define OK (0)
 
-enum {
-	TINYLIB_WM_APP_INITDONE = WM_APP, /* unblock rendering thread */
-	TINYLIB_WM_APP_PAUSE, /* pauses rendering */
-	TINYLIB_WM_APP_CONTINUE, /* continues rendering */
-	TINYLIB_WM_APP_STOP, /* exit rendering thread */
-	TINYLIB_WM_APP_RESIZE, /* resize viewport */
-};
+static BOOL quit = FALSE;
+static int width = 800, height = 600;
+static LONGLONG qpcFrequency;
+static EGLDisplay eglDisplay;
+static EGLSurface eglSurface;
 
-#ifndef TINYLIB_MAX_WINDOWS
-#define TINYLIB_MAX_WINDOWS 5
-#endif
-
-typedef void (*render_func_t)(double elapsed, unsigned width, unsigned height);
-
-struct win_info {
-	BOOL alive;
-	HWND hWnd;
-	render_func_t render_cb;
-	BOOL bFullscreen;
-};
-
-static int current_index = TINYLIB_ERR;
-static struct win_info window[TINYLIB_MAX_WINDOWS];
-static INT config_min_width, config_min_height;
-static UINT _nRenderThreadID;
-
-static void
-render_pause(void)
+static void errormsg(const char *m, ...)
 {
-	// TODO: PostThreadMessage(_nRenderThreadID, TINYLIB_WM_APP_PAUSE, TRUE, 0);
+	static char buf[1024];
+	va_list ap;
+	va_start(ap, m);
+	snprintf(buf, sizeof(buf), m, ap);
+	va_end(ap);
+	MessageBoxA(NULL, buf, "Error", MB_OK);
 }
 
-static void
-render_continue(void)
+static void setup(void)
 {
-	// TODO: PostThreadMessage(_nRenderThreadID, TINYLIB_WM_APP_CONTINUE, FALSE, 0);
+	printf("Initializing scene ...\n");
+//	glEnable(GL_DEPTH_TEST);
+	glClearColor(0.1f, 0.9f, 0.9f, 1.0f);
 }
 
-static int
-find_next_window(void)
+static void paint(double timeFactor)
 {
-	int i;
-	for (i = 0; i < TINYLIB_MAX_WINDOWS; i++) {
-		if (window[i].alive == FALSE) {
-			return i;
-		}
-	}
+	(void)timeFactor;
 
-	return TINYLIB_ERR;
-}
-
-static int
-find_win(HWND hWnd) {
-	int i;
-	for (i = 0; i < TINYLIB_MAX_WINDOWS; i++) {
-		if (window[i].alive == TRUE && window[i].hWnd == hWnd) {
-			return i;
-		}
-	}
-
-	return TINYLIB_ERR;
-}
-
-static LRESULT CALLBACK
-MainWndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
-	int cur = find_win(hWnd);
-	switch (Msg) {
-
-	case TINYLIB_WM_APP_INITDONE:
-		/* render thread is ready */
-		if (wParam) { /* full screen window? */
-			ShowWindow(hWnd, SW_MAXIMIZE);
-		} else {
-			ShowWindow(hWnd, SW_SHOW);
-		}
-
-		/* take the focus */
-		SetForegroundWindow(hWnd);
-		SetFocus(hWnd);
-
-		return 1; /* handled */
-
-	case WM_GETMINMAXINFO:
-		/* restrict window resize if a mininum size is set */
-		if (config_min_height > 0 && config_min_width > 0) {
-			MINMAXINFO *pInfo = (MINMAXINFO *)lParam;
-			pInfo->ptMinTrackSize.x = config_min_width;
-			pInfo->ptMinTrackSize.y = config_min_height;
-		}
-		break;
-
-	case WM_SIZE:
-
-		/* pause rendering on minimize */
-		switch (wParam) {
-		case SIZE_MINIMIZED:
-			/* pause rendering while minimized */
-			render_pause();
-			break;
-
-		case SIZE_RESTORED:
-			/* tell the render thread to resize viewport */
-			PostThreadMessage(_nRenderThreadID, TINYLIB_WM_APP_RESIZE, 0, 0);
-
-			/* unblock render thread */
-			render_continue();
-			break;
-		}
-
-		break;
-
-	case WM_ENTERMENULOOP:
-		/* suspend while a menu is accessed */
-		if (!IsIconic(hWnd)) {
-			render_pause();
-		}
-		break;
-
-	case WM_EXITMENULOOP:
-		/* restore from the menu triggered suspend */
-		if (!IsIconic(hWnd)) {
-			render_continue();
-		}
-		break;
-
-	case WM_KEYDOWN:
-		break;
-
-	case WM_CLOSE:
-		// TODO: save window size and position
-
-		/* terminate render thread */
-		// TODO: PostThreadMessage(_nRenderThreadID, TINYLIB_WM_APP_STOP, 0, 0);
-		// TODO: WaitForSingleObject(_hRenderThread, 5000);
-
-		DestroyWindow(hWnd);
-		break;
-
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-
-	case WM_PAINT: // TODO: Force a redraw through the render thread
-		if (cur != TINYLIB_ERR && window[cur].render_cb) {
-			// PAINTSTRUCT ps;
-			// HDC hdc = BeginPaint(hWnd, &ps);
-			// window[cur].render_cb(hWnd, hdc, &ps.rcPaint);
-			// EndPaint(hWnd, &ps);
-			window[cur].render_cb(0.0, 0, 0); // TODO: get elapsed time and width/height
-		}
-		return 0;
-	}
-
-	return DefWindowProc(hWnd, Msg, wParam, lParam);
-}
-
-static int
-newwin(const char *title, int width, int height, render_func_t render_func)
-{
-	static ATOM wndcls;
-	static HINSTANCE hInstance;
-
-	if (!hInstance) {
-		hInstance = GetModuleHandle(NULL);
-	}
-
-	if (!wndcls) {
-		WNDCLASSEX wcx = { 0 };
-
-		wcx.cbSize = sizeof(wcx);
-		wcx.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-		wcx.lpfnWndProc = MainWndProc;
-		wcx.cbClsExtra = 0;
-		wcx.cbWndExtra = 0;
-		wcx.hInstance = hInstance;
-		wcx.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-		wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
-		wcx.hbrBackground = GetStockObject(WHITE_BRUSH);
-		wcx.lpszMenuName = NULL;
-		wcx.lpszClassName = TEXT(__FILE__);
-		wcx.hIconSm = LoadImage(hInstance, MAKEINTRESOURCE(5),
-					IMAGE_ICON,
-					GetSystemMetrics(SM_CXSMICON),
-					GetSystemMetrics(SM_CYSMICON),
-					LR_DEFAULTCOLOR);
-
-		wndcls = RegisterClassEx(&wcx);
-		if (!wndcls) {
-			MessageBox(NULL, TEXT("RegisterClassEx() failed: Cannot register window class."), TEXT("Error"), MB_OK);
-			return TINYLIB_ERR;
-		}
-	}
-
-	int win_index = find_next_window();
-
-	if (win_index == TINYLIB_ERR) {
-		MessageBox(NULL, TEXT("Too many open windows."), TEXT("Error"), MB_OK);
-		return TINYLIB_ERR;
-	}
-
-#ifdef UNICODE
-        static WCHAR title_buf[256];
-        int len;
-        len = MultiByteToWideChar(CP_UTF8, 0, title, -1, title_buf, sizeof(title_buf) / sizeof(*title_buf));
-        if (!len) {
-		title_buf[0] = 0; // ERROR
-		MessageBox(NULL, TEXT("Invalidate window title string"), TEXT("Error"), MB_OK);
-		return TINYLIB_ERR;
-	}
-#else
-        char *title_buf = title; // pass it through
-	// TODO: utf8 to local encoding
-#endif
-
-	HWND hwnd = CreateWindow(MAKEINTATOM(wndcls), title_buf,
-		WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		width, height, NULL, NULL, hInstance, NULL);
-
-	if (hwnd == NULL) {
-		MessageBox(NULL, TEXT("CreateWindow() failed: Cannot create OpenGL window."), TEXT("Error"), MB_OK);
-		return TINYLIB_ERR;
-	}
-
-	HDC hdc = GetDC(hwnd);
-
-	PIXELFORMATDESCRIPTOR pfd = { 0 };
-	pfd.nSize        = sizeof(pfd);
-	pfd.nVersion     = 1;
-	pfd.dwFlags      = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL;
-	pfd.iPixelType   = PFD_TYPE_RGBA;
-	pfd.cColorBits   = 32;
-
-	int pf = ChoosePixelFormat(hdc, &pfd);
-
-	SetPixelFormat(hdc, pf, &pfd);
-
-	DescribePixelFormat(hdc, pf, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
-
-	ReleaseDC(hwnd, hdc);
-
-	// TODO: initialize bootstrap context in this thread
-
-	// TODO: create real window for the new context that will be created by render thread
-
-	/* add the new window to the list */
-	window[win_index] = (struct win_info){ .hWnd = hwnd, .render_cb = render_func };
-
-	/* select the new window */
-	current_index = win_index;
-
-	// TODO: create new context
-
-	// TODO: move GL context creation to this thread
-	// _hRenderThread = (HANDLE)_beginthreadex(NULL, 0, RenderMain, &args, 0, &_nRenderThreadID);
-
-	// TODO: clean up old window and old context
-
-	return win_index;
-}
-
-static int
-delwin(int win_index) {
-	if (win_index < 0 || win_index >= TINYLIB_MAX_WINDOWS || !window[win_index].alive) {
-		return TINYLIB_ERR;
-	}
-
-	DestroyWindow(window[win_index].hWnd);
-	window[win_index].hWnd = NULL; // TODO: move into WM_DESTROY handler
-	window[win_index].alive = FALSE;
-	if (current_index == win_index) {
-		current_index = TINYLIB_ERR;
-	}
-
-	return TINYLIB_OK;
-}
-
-/* selects a window for future operations */
-static int
-selectwin(int win_index)
-{
-	if (win_index < 0 || win_index >= TINYLIB_MAX_WINDOWS || !window[win_index].alive) {
-		return TINYLIB_ERR;
-	}
-
-	current_index = win_index;
-
-	return TINYLIB_OK;
-}
-
-static int
-loop(void)
-{
-	MSG msg;
-
-	while (GetMessage(&msg, NULL, 0, 0) != 0) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	// TODO: if (_hRenderThread != NULL) CloseHandle(_hRenderThread);
-
-	return (int)msg.wParam;
-}
-
-
-static void
-my_paint(double elapsed, unsigned width, unsigned height)
-{
-	(void)elapsed;
-	(void)width;
-	(void)height;
-
-	glClearColor(1.0, 1.0, 0.0, 1.0);
+	glViewport(0, 0, width, height);
+//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	// glDrawArrays(GL_TRIANGLE_STRIP, 0, 26);
+
 	glFlush();
 }
 
-int WINAPI
-wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PWSTR pCmdLine, int nCmdShow)
+LRESULT CALLBACK wndProc(HWND hwnd, unsigned int msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg) {
+	case WM_CLOSE :
+		quit = TRUE;
+		DestroyWindow(hwnd);
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_SIZE:
+		width = LOWORD(lParam);
+		height = HIWORD(lParam);
+		return 0;
+	}
+
+	return (DefWindowProc(hwnd, msg, wParam, lParam));
+}
+
+static int newwin(void)
+{
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_OWNDC;
+	wcex.lpfnWndProc = &DefWindowProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = NULL;
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = 0;
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = L"eglsamplewnd";
+	wcex.hIconSm = NULL;
+	wcex.lpfnWndProc = wndProc;
+
+	RegisterClassEx(&wcex);
+	RECT rect = { 0, 0, width, height };
+	int style = WS_BORDER | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME;
+	AdjustWindowRect(&rect, style, FALSE);
+
+	HWND hwnd = CreateWindow(L"eglsamplewnd", L"EGL OpenGL ES 2.0 example", style, CW_USEDEFAULT, CW_USEDEFAULT, rect.right - rect.left, rect.bottom - rect.top, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	HDC hdc = GetDC(hwnd);
+	EGLDisplay eglDisplay = eglGetDisplay(hdc);
+	if (eglDisplay == EGL_NO_DISPLAY) {
+		errormsg("No EGL display");
+		return ERR;
+	}
+
+	EGLint eglVersionMajor, eglVersionMinor;
+	eglInitialize(eglDisplay, &eglVersionMajor, &eglVersionMinor);
+	eglBindAPI(EGL_OPENGL_ES_API);
+
+	EGLint configAttributes[] = {
+		EGL_BUFFER_SIZE, 0,
+		EGL_RED_SIZE, 5,
+		EGL_GREEN_SIZE, 6,
+		EGL_BLUE_SIZE, 5,
+		EGL_ALPHA_SIZE, 0,
+		EGL_COLOR_BUFFER_TYPE, EGL_RGB_BUFFER,
+		EGL_CONFIG_CAVEAT, EGL_DONT_CARE,
+		EGL_CONFIG_ID, EGL_DONT_CARE,
+		EGL_DEPTH_SIZE, 24,
+		EGL_LEVEL, 0,
+		EGL_MAX_SWAP_INTERVAL, EGL_DONT_CARE,
+		EGL_MIN_SWAP_INTERVAL, EGL_DONT_CARE,
+		EGL_NATIVE_RENDERABLE, EGL_DONT_CARE,
+		EGL_NATIVE_VISUAL_TYPE, EGL_DONT_CARE,
+		EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+		EGL_SAMPLE_BUFFERS, 0,
+		EGL_SAMPLES, 0,
+		EGL_STENCIL_SIZE, 0,
+		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+		EGL_TRANSPARENT_TYPE, EGL_NONE,
+		EGL_TRANSPARENT_RED_VALUE, EGL_DONT_CARE,
+		EGL_TRANSPARENT_GREEN_VALUE, EGL_DONT_CARE,
+		EGL_TRANSPARENT_BLUE_VALUE, EGL_DONT_CARE,
+		EGL_NONE
+	};
+
+	EGLint surfaceAttributes[] = { EGL_NONE };
+	EGLint contextAttributes[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
+
+	EGLint nrOfConfigs;
+	EGLConfig windowConfig;
+	if (!eglChooseConfig(eglDisplay, configAttributes, &windowConfig, 1, &nrOfConfigs)) {
+		errormsg("No valid EGL configs");
+		// TODO: tear down eglDisplay and hwnd
+		return ERR;
+	}
+
+	EGLSurface eglSurface = eglCreateWindowSurface(eglDisplay, windowConfig, hwnd, surfaceAttributes);
+	if (!nrOfConfigs || eglSurface == EGL_NO_SURFACE) {
+		errormsg("Could not create EGL surface");
+		// TODO: tear down eglDisplay and hwnd
+		return ERR;
+	}
+
+	EGLContext eglContext = eglCreateContext(eglDisplay, windowConfig, NULL, contextAttributes);
+
+	if (eglContext == EGL_NO_CONTEXT) {
+		errormsg("Could not create EGL context");
+		// TODO: tear down eglSurface, eglDisplay and hwnd
+		return ERR;
+	}
+
+	eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext);
+
+	setup();
+
+	return OK;
+}
+
+static int init(void)
+{
+	QueryPerformanceCounter((LARGE_INTEGER*)&qpcFrequency);
+
+	if (newwin() != OK) {
+		return ERR;
+	}
+
+	return OK;
+}
+
+static void loop(void)
+{
+	double timeFactor = 1.0f;
+	MSG uMsg;
+
+#if 0
+	if (PeekMessage(&uMsg, NULL, 0, 0, PM_REMOVE) > 0) {
+		TranslateMessage(&uMsg);
+		DispatchMessage(&uMsg);
+	}
+#endif
+
+	LONGLONG qpcStart, qpcEnd;
+
+	while (!quit)  {
+		QueryPerformanceCounter((LARGE_INTEGER*)&qpcStart);
+
+		printf("Painting...\n");
+		paint(timeFactor);
+
+#if 0
+		while (PeekMessage(&uMsg, NULL, 0, 0, PM_REMOVE) > 0) {
+			printf("MSG %d\n", uMsg.message);
+			TranslateMessage(&uMsg);
+			DispatchMessage(&uMsg);
+		}
+#else
+		while (GetMessage(&uMsg, NULL, 0, 0)) {
+			printf("MSG %d\n", uMsg.message);
+			TranslateMessage(&uMsg);
+			DispatchMessage(&uMsg);
+		}
+#endif
+
+		eglSwapBuffers(eglDisplay, eglSurface);
+
+		QueryPerformanceCounter((LARGE_INTEGER*)&qpcEnd);
+		double dTime = (double)(qpcEnd - qpcStart) / (double)qpcFrequency;
+		timeFactor += dTime * 0.01f;
+	}
+}
+
+static void done(void)
+{
+	// TODO:
+}
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
 	(void)hInstance;
-	(void)prevInstance;
-	(void)pCmdLine;
+	(void)hPrevInstance;
+	(void)lpCmdLine;
 	(void)nCmdShow;
 
-	newwin(__FILE__, 1024, 768, my_paint);
+	if (init() != OK) {
+		return 1;
+	}
 
-	// TODO: test selectwin();
-	// TODO: test delwin();
+	loop();
+	done();
 
-	return loop();
+	return 0;
 }
