0cbb6f0d856cefbe349224d9c215009f8e5d9b2d
Jon Mayo <jon@rm-f.net>
2023-05-11 18:16:59 -0700

    add zip_load and gif_load

--- src/thirdparty/update-source.sh
+++ src/thirdparty/update-source.sh
@@ -10,3 +10,9 @@ curl -L \
   -O https://raw.githubusercontent.com/nothings/stb/master/stb_image_write.h \
   -O https://raw.githubusercontent.com/nothings/stb/master/stb_ds.h \
   -O https://raw.githubusercontent.com/nothings/stb/master/LICENSE
+
+# git_load
+curl -L -O https://raw.githubusercontent.com/hidefromkgb/gif_load/master/gif_load.h
+
+# zip_load
+curl -L -O https://raw.githubusercontent.com/hidefromkgb/zip_load/master/zip_load.h
